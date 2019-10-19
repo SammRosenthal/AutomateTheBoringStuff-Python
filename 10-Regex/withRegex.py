e06615f5c708a6d56d0a1d492d8a7f7238b3be1e
Sam Rosenthal <samrrosenthal@gmail.com>
2019-10-19 12:42:24 -0500

    finished adding groups to my regex

--- 10-Regex/withRegex.py
+++ 10-Regex/withRegex.py
@@ -9,3 +9,21 @@ print(mo.group())
 moAll = phoneNumRegex.findall(message)
 print(moAll, sep=', ')
 
+
+message1 = 'Call me 415-555-1011 tomorrow, or at 415-555-9999.'
+# putting () around sections of regex will make groups
+# you can see me printing only specific groups below
+testRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo1 = testRegex.search(message1)
+
+print(mo1.group(1))
+print(mo1.group(2))
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel')
+
+print(mo.group())
+# putting just group() will return the full matching string 
+# putting group(1) will return the portion of the group that was matched
+# NOT the full string
+print(mo.group(1))
