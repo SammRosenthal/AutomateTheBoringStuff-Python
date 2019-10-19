6ab3c24e377192444d66caeed9236ecf2dc7b6a0
Sam Rosenthal <samrrosenthal@gmail.com>
2019-10-19 12:53:15 -0500

    added more notes on the *?+ symbols for regex

--- 10-Regex/withRegex.py
+++ 10-Regex/withRegex.py
@@ -23,7 +23,28 @@ batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
 mo = batRegex.search('Batmobile lost a wheel')
 
 print(mo.group())
-# putting just group() will return the full matching string 
+# putting just group() will return the full matching string                                                                                                                                                                                                                                                                                                                                         
 # putting group(1) will return the portion of the group that was matched
 # NOT the full string
 print(mo.group(1))
+
+# the ? says this group can appear 0 or 1 times to match the pattern
+# this is the same as writing re.compile(r'Batwoman|Batman')
+batRegex = re.compile(r'Bat(wo)?man')
+
+mo = batRegex.search('The Adventures of Batman')
+print(mo.group())
+mo = batRegex.search('The Adventures of Batwoman')
+print(mo.group())
+
+# this will make the area code optional when searching for a phonenumber
+phoneNumRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
+
+# the * is like the ? but instead can appear 0 or anynumber of times instead of just 0 or 1 time
+batRegex = re.compile(r'Bat(wo)*man')
+
+mo = batRegex.search('The adventure of Batwowowowowowowowowowowoman')
+print(mo.group())
+
+haRegex = re.compile(r'(Ha){3}')
+print(haRegex.search('he said HaHaHa').group())
