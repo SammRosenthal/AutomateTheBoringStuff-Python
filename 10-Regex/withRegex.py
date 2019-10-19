897f01f2f95e08fdee1e400afb89c043925db4f7
Sam Rosenthal <samrrosenthal@gmail.com>
2019-10-19 12:35:49 -0500

    used regex to rewrite the phonenumber search i wrote earlier

---
+++ 10-Regex/withRegex.py
@@ -0,0 +1,11 @@
+import re
+
+message = 'Call me 415-555-1011 tomorrow, or at 415-555-9999.'
+
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search(message)
+print(mo.group())
+
+moAll = phoneNumRegex.findall(message)
+print(moAll, sep=', ')
+
