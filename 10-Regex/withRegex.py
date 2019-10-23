f4c5fc2a8f81c833a2bd4232a6d89542f826c0e2
Sam Rosenthal <samrrosenthal@gmail.com>
2019-10-23 18:57:06 -0500

    updating so can pull onto laptop

--- 10-Regex/withRegex.py
+++ 10-Regex/withRegex.py
@@ -53,3 +53,45 @@ print(haRegex.search('he said HaHaHa').group())
 # putting a ? after the {} will make the search non greedy
 # greedy matching makes regex look for the longest possible string to match
 # non-greedy matching matches the shortest string possible
+
+
+phoneNumRegex = re.compile(r'(\d\d\d)(-)(\d\d\d-\d\d\d\d)')
+
+exampleResume = """234 East Speedway Blvd., Tucson, AZ 85719
+(480) 452-5337
+linda_brown@gmail.com
+
+Sales Associate with 5 years of experience in retail environments. Recognized for ability to communicate with customers, providing exceptional service that ensure client retention and positive feedback. Proven ability to increase sales through upselling techniques as well as implementing processes that drive profitability.
+
+PROFESSIONAL EXPERIENCE
+H&M – Tucson, AZ
+Sales Associate, June 2013 – Present
+
+Assist an average of 40 customers per day in finding or selecting items, and provided recommendations that generated $8K in additional revenue
+Stock, replenish, and organize inventory with accuracy and efficiency, completing function 10% faster than average associate
+Achieve an average of 140% of sales goals for three consecutive months
+Manage proper and attractive merchandise display, ensuring strategic placement of products that maximized purchases
+Provide outstanding customer service, receiving 96% in customer service feedback surveys
+TARGET – Phoenix, AZ
+Sales Associate, Oct 2010 – May 2013
+
+Helped an average of 50 customers per day by responding to inquiries and finding products
+Recommended better merchandise display to management, which was implemented and resulted in 35% improved sales
+Assisted team members when necessary in handling cash registers, organizing inventory room, labeling products, unloading merchandise, and cleaning up
+Aided Spanish-speaking customers, increasing repeat and loyal customers by 30%
+EDUCATION
+GATEWAY COMMUNITY COLLEGE – Phoenix, AZ
+Associate of Arts in Humanities, June 2008
+
+Member of Student Activities Management (SAM)
+Vice President of Women’s Forum
+ADDITIONAL SKILLS
+MS Office
+Bilingual in English and Spanish
+Retail Software
+Social Media"""
+
+
+print(phoneNumRegex.findall(exampleResume))
+
+
